e14efdbbff834f09f116e0ff18f709a1ab11c935
Merfrei <contact@merfrei.com>
2024-02-15 19:29:38 -0300

    Add boost and bump

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 300f;
-    [SerializeField] float moveSpeed = 10f;
-
-    void Start()
-    {
-
-    }
+    [SerializeField] float moveSpeed = 15f;
+    [SerializeField] float slowSpeed = 10f;
+    [SerializeField] float boostSpeed = 20f;
 
     void Update()
     {
@@ -18,5 +15,17 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Boost"))
+        {
+            this.moveSpeed = this.boostSpeed;
+        }
+        else if (other.CompareTag("Bump"))
+        {
+            this.moveSpeed = this.slowSpeed;
+        }
     }
 }
